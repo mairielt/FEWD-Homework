ce868df2ff9437232521663f38f793e2f082d302
leigh james <leighjames@leighs-MacBook-Air.local>
2018-04-19 20:43:39 -0700

    Citipix update JS

--- WEEK 06/CITIPIX/js/app.js
+++ WEEK 06/CITIPIX/js/app.js
@@ -3,32 +3,32 @@ $(document).ready(function(){
 
     function updateCity(){
         // grab the value in the city text box, store it as the city variable:
-        var city = $("#city-type").val()
+        var city = $("#city-type").val().trim().toLowerCase()
 
         // output city to the js console:
         console.log(city)
 
-        // if city is "la", change the backgrond to images/la.jpg:
-        if(city == "la"){
+        // if city is Los Angeles, change the backgrond to images/la.jpg:
+        if(city == "la" || city =="los angeles"){
             $("body").attr("class", "la")
         }
 
-        // if city is "nyc", change the backgrond to images/nyc.jpg:
-        if(city == "nyc"){
+        // if city is New York, change the backgrond to images/nyc.jpg:
+        if(city == "nyc" || city =="new york"){
             $("body").attr("class", "nyc")
         }
 
-        // if city is "austin", change the backgrond to images/austin.jpg:
+        // if city is Austin, change the backgrond to images/austin.jpg:
         if(city == "austin"){
             $("body").attr("class", "austin")
         }
 
-        // if city is "sf", change the backgrond to images/sf.jpg:
-        if(city == "sf"){
+        // if city is San Francisco, change the backgrond to images/sf.jpg:
+        if(city == "sf" || city =="san francisco"){
             $("body").attr("class", "sf")
         }
 
-        // if city is "sydney", change the backgrond to images/sydney.jpg:
+        // if city is Sydney, change the backgrond to images/sydney.jpg:
         if(city == "sydney"){
             $("body").attr("class", "sydney")
         }
