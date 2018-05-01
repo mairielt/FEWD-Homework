bf66541dbf217cb88f08d5ebb6db0315f7ad2923
leigh james <leighjames@leighs-MacBook-Air.local>
2018-05-01 14:36:15 -0700

    Relaxr Responsive Homework

--- WEEK 07/RESPONSIVE DESIGN/js/app.js
+++
@@ -1,8 +0,0 @@
-// This waits till the page finishes loading before running the code inside the {}
-$(document).ready(function() {
-
-  console.log("If you see this message, jQuery is working.")
-  
-  // Your code goes here
-
-});
